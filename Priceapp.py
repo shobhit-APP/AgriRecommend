dd10879e2930528bdb38435809554376e0c60d14
Shobhit Srivastava <shobhitsrivastava2004@gmail.com>
2025-01-31 13:59:10 +0530

    Update Priceapp.py

--- Priceapp.py
+++ Priceapp.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 import xgboost as xgb
-from keras.models import Sequential, load_model
+from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense
 from sklearn.model_selection import train_test_split
 from flask import Flask, request, jsonify
@@ -124,7 +124,8 @@ def predict():
             new_data['market'] = encode_column('market', market_encoder)
             new_data['crop_name'] = encode_column('crop_name', crop_name_encoder)
         except ValueError as e:
-            return jsonify({'error': f'Encoding error: {str(e)}')}), 400
+            response = jsonify({'error': f'Encoding error: {str(e)}'})
+            return response, 400
 
         predicted_price_xgb = xgb_model.predict(new_data)
         predicted_price_xgb = float(predicted_price_xgb[0])
@@ -136,7 +137,8 @@ def predict():
             'predicted_price_nn': predicted_price_nn
         })
     except Exception as e:
-        return jsonify({'error': f'Unexpected error: {str(e)}')}), 500
+        response = jsonify({'error': f'Unexpected error: {str(e)}'})
+        return response, 500
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Get PORT from Render, default to 5000
